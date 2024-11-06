250db9ab5eea7aad9c50e464ee0e165068708459
Hasafif <hassan.n.afif@gmail.com>
2024-11-06 00:05:43 -0500

    f7

--- next.config.js
+++ next.config.js
@@ -10,6 +10,19 @@ const nextConfig = {
     domains: ['firebasestorage.googleapis.com','gyzedbhfnyygctzl.public.blob.vercel-storage.com'],
 
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://e-commerce-platform-6x26-8dpwg9bwr-hasan-afifs-projects.vercel.app' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, PATCH, DELETE, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
