1f7eb5dd7340b2d631414483cfaaa1d8d894e536
Hasafif <hassan.n.afif@gmail.com>
2024-11-06 03:48:59 -0500

    f17

--- src/Services/common/product/index.ts
+++ src/Services/common/product/index.ts
@@ -1,10 +1,11 @@
 import Cookies from "js-cookie";
-//export const origin = 'https://e-commerce-platform-6x26.vercel.app'
+let origin = 'https://e-commerce-platform-6x26.vercel.app'
+//origin = 'http://localhost:3000'
 export const get_product_details = async (id:string) => {
     const formdata = new FormData();
     formdata.append('id',id)
     try {
-      const res = await fetch(`/api/common/product/get-product-details?id=${id}`, {
+      const res = await fetch(`${origin}/api/common/product/get-product-details?id=${id}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
